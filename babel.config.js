4f5f5b8d960616ab1aab8759bad1e71e8dedf348
杨介正 <hzyangjiezheng@corp.netease.com>
2020-03-27 14:51:43 +0800

    feat: template

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  sourceType: 'unambiguous',
+  'presets': [
+    ['@babel/preset-env', {
+      'modules': false,
+      'useBuiltIns': 'usage',
+      'corejs': 3
+    }],
+    ['@babel/preset-react', {}]
+  ],
+  'plugins': [
+    ['@babel/plugin-proposal-decorators', {'legacy': true}],
+    ['@babel/plugin-syntax-dynamic-import'],
+    ['@babel/plugin-proposal-class-properties']
+  ]
+}
